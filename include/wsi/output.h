ec220e8cd6209d99514a5935b870fae62bf4fa43
Mark Bolhuis <mark@bolhuis.dev>
2022-09-06 17:16:03 +0100

    Enclose wsiOutputCreate in extern block

--- include/wsi/output.h
+++ include/wsi/output.h
@@ -3,7 +3,15 @@
 
 #include "common.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 WsiResult
 wsiOutputCreate(WsiPlatform platform, WsiOutput *pOutput);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
