30c38ecf91451620412260d40cbbf1a2a9289132
Mark Bolhuis <mark@bolhuis.dev>
2022-09-13 17:33:27 +0100

    xcb: Add event_queue.c

---
+++ src/xcb/event_queue.c
@@ -0,0 +1,21 @@
+#include <xcb/xcb.h>
+
+#include "wsi/event_queue.h"
+
+WsiResult
+wsiCreateEventQueue(WsiPlatform platform, WsiEventQueue *pEventQueue)
+{
+    return WSI_SUCCESS;
+}
+
+void
+wsiDestroyEventQueue(WsiPlatform platform, WsiEventQueue eventQueue)
+{
+
+}
+
+WsiResult
+wsiPollEventQueue(WsiEventQueue eventQueue)
+{
+    return WSI_ERROR_NOT_IMPLEMENTED;
+}
