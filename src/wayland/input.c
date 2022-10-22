715f4885d673781d316cfb6699a632a7c1ea05b1
Mark Bolhuis <mark@bolhuis.dev>
2022-10-22 13:03:06 +0100

    wayland: Consolidate pointer and keyboard in input.c

--- src/wayland/input.c
+++ src/wayland/input.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #include <wayland-client-protocol.h>
+#include <wayland-cursor.h>
 
 #include <xkbcommon/xkbcommon.h>
 
@@ -10,10 +13,460 @@
 
 #include "platform_priv.h"
 #include "input_priv.h"
-#include "pointer_priv.h"
-#include "keyboard_priv.h"
 
 #define WSI_WL_SEAT_VERSION 7
+
+static void
+wsi_pointer_set_cursor(
+    struct wsi_pointer *ptr,
+    const char *name,
+    uint32_t serial)
+{
+    ptr->wl_cursor = wl_cursor_theme_get_cursor(ptr->wl_cursor_theme, name);
+
+    struct wl_buffer *buffer = NULL;
+    int32_t x = 0, y = 0;
+    if (ptr->wl_cursor != NULL) {
+        struct wl_cursor_image *image = ptr->wl_cursor->images[0];
+        buffer = wl_cursor_image_get_buffer(image);
+        x = (int32_t)image->hotspot_x;
+        y = (int32_t)image->hotspot_y;
+    }
+
+    wl_surface_attach(ptr->wl_cursor_surface, buffer, 0, 0);
+    wl_surface_commit(ptr->wl_cursor_surface);
+
+    struct wl_surface *surface = ptr->wl_cursor
+                               ? ptr->wl_cursor_surface
+                               : NULL;
+    wl_pointer_set_cursor(ptr->wl_pointer, serial, surface, x, y);
+}
+
+static void
+wsi_pointer_frame(struct wsi_pointer *pointer)
+{
+    if (pointer->frame.mask & WSI_POINTER_EVENT_ENTER) {
+        wsi_pointer_set_cursor(pointer, "left_ptr", pointer->frame.serial);
+    }
+
+    pointer->frame.mask = WSI_POINTER_EVENT_NONE;
+}
+
+// region Wl Pointer
+
+static void
+wl_pointer_enter(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t serial,
+    struct wl_surface *surface,
+    wl_fixed_t sx,
+    wl_fixed_t sy)
+{
+    struct wsi_pointer *pointer = data;
+
+    pointer->frame.mask |= WSI_POINTER_EVENT_ENTER;
+    pointer->frame.serial = serial;
+    pointer->frame.enter = surface;
+    pointer->frame.x = wl_fixed_to_double(sx);
+    pointer->frame.y = wl_fixed_to_double(sy);
+
+    if (wl_pointer_get_version(wl_pointer) < WL_POINTER_FRAME_SINCE_VERSION) {
+        wsi_pointer_frame(pointer);
+    }
+}
+
+static void
+wl_pointer_leave(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t serial,
+    struct wl_surface *surface)
+{
+    struct wsi_pointer *pointer = data;
+
+    pointer->frame.mask |= WSI_POINTER_EVENT_LEAVE;
+    pointer->frame.serial = serial;
+    pointer->frame.leave = surface;
+
+    if (wl_pointer_get_version(wl_pointer) < WL_POINTER_FRAME_SINCE_VERSION) {
+        wsi_pointer_frame(pointer);
+    }
+}
+
+static void
+wl_pointer_motion(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t time,
+    wl_fixed_t sx,
+    wl_fixed_t sy)
+{
+    struct wsi_pointer *pointer = data;
+
+    pointer->frame.mask |= WSI_POINTER_EVENT_MOTION;
+    pointer->frame.time = time;
+    pointer->frame.x = wl_fixed_to_double(sx);
+    pointer->frame.y = wl_fixed_to_double(sy);
+
+    if (wl_pointer_get_version(wl_pointer) < WL_POINTER_FRAME_SINCE_VERSION) {
+        wsi_pointer_frame(pointer);
+    }
+}
+
+static void
+wl_pointer_button(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t serial,
+    uint32_t time,
+    uint32_t button,
+    uint32_t state)
+{
+    struct wsi_pointer *pointer = data;
+
+    pointer->frame.mask |= WSI_POINTER_EVENT_BUTTON;
+    pointer->frame.serial = serial;
+    pointer->frame.time = time;
+    pointer->frame.button = button;
+    pointer->frame.state = state;
+
+    if (wl_pointer_get_version(wl_pointer) < WL_POINTER_FRAME_SINCE_VERSION) {
+        wsi_pointer_frame(pointer);
+    }
+}
+
+static void
+wl_pointer_axis(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t time,
+    uint32_t axis,
+    wl_fixed_t value)
+{
+    struct wsi_pointer *pointer = data;
+
+    pointer->frame.mask |= WSI_POINTER_EVENT_AXIS;
+    pointer->frame.axes[axis].start_time = time;
+    pointer->frame.axes[axis].value = wl_fixed_to_double(value);
+
+    if (wl_pointer_get_version(wl_pointer) < WL_POINTER_FRAME_SINCE_VERSION) {
+        wsi_pointer_frame(pointer);
+    }
+}
+
+static void
+wl_pointer_frame(
+    void *data,
+    struct wl_pointer *wl_pointer)
+{
+    struct wsi_pointer *pointer = data;
+
+    wsi_pointer_frame(pointer);
+}
+
+static void
+wl_pointer_axis_source(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t axis_source)
+{
+    struct wsi_pointer *pointer = data;
+
+    pointer->frame.mask |= WSI_POINTER_EVENT_AXIS_SOURCE;
+    pointer->frame.axis_source = axis_source;
+}
+
+static void
+wl_pointer_axis_stop(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t time,
+    uint32_t axis)
+{
+    struct wsi_pointer *pointer = data;
+
+    pointer->frame.mask |= WSI_POINTER_EVENT_AXIS_STOP;
+    pointer->frame.axes[axis].stop_time = time;
+}
+
+static void
+wl_pointer_axis_discrete(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t axis,
+    int32_t discrete)
+{
+    if (wl_pointer_get_version(wl_pointer) <
+        WL_POINTER_AXIS_VALUE120_SINCE_VERSION)
+    {
+        struct wsi_pointer *pointer = data;
+
+        pointer->frame.mask |= WSI_POINTER_EVENT_AXIS_DISCRETE;
+        pointer->frame.axes[axis].discrete = discrete * 120;
+    }
+}
+
+static void
+wl_pointer_axis_value120(
+    void *data,
+    struct wl_pointer *wl_pointer,
+    uint32_t axis,
+    int32_t value)
+{
+    struct wsi_pointer *pointer = data;
+
+    pointer->frame.mask |= WSI_POINTER_EVENT_AXIS_DISCRETE;
+    pointer->frame.axes[axis].discrete = value;
+}
+
+static const struct wl_pointer_listener wl_pointer_listener = {
+    .enter = wl_pointer_enter,
+    .leave = wl_pointer_leave,
+    .motion = wl_pointer_motion,
+    .button = wl_pointer_button,
+    .axis = wl_pointer_axis,
+    .frame = wl_pointer_frame,
+    .axis_source = wl_pointer_axis_source,
+    .axis_stop = wl_pointer_axis_stop,
+    .axis_discrete = wl_pointer_axis_discrete,
+    .axis_value120 = wl_pointer_axis_value120,
+};
+
+// endregion
+
+static bool
+wsi_pointer_create(struct wsi_seat *seat)
+{
+    assert(seat->pointer == NULL);
+
+    struct wsi_pointer *ptr = calloc(1, sizeof(struct wsi_pointer));
+    if (ptr == NULL) {
+        return false;
+    }
+
+    struct wsi_platform *plat = seat->global.platform;
+
+    ptr->seat = seat;
+
+    ptr->wl_pointer = wl_seat_get_pointer(seat->wl_seat);
+    wl_pointer_add_listener(ptr->wl_pointer, &wl_pointer_listener, ptr);
+
+    ptr->wl_cursor_theme = wl_cursor_theme_load(NULL, 24, plat->wl_shm);
+    ptr->wl_cursor_surface = wl_compositor_create_surface(plat->wl_compositor);
+
+    seat->pointer = ptr;
+
+    return true;
+}
+
+static void
+wsi_pointer_destroy(struct wsi_seat *seat)
+{
+    assert(seat->pointer != NULL);
+
+    struct wsi_pointer *ptr = seat->pointer;
+    seat->pointer = NULL;
+
+    if (wl_pointer_get_version(ptr->wl_pointer) >=
+        WL_POINTER_RELEASE_SINCE_VERSION)
+    {
+        wl_pointer_release(ptr->wl_pointer);
+    } else {
+        wl_pointer_destroy(ptr->wl_pointer);
+    }
+
+    wl_cursor_theme_destroy(ptr->wl_cursor_theme);
+    wl_surface_destroy(ptr->wl_cursor_surface);
+
+    free(ptr);
+}
+
+static void
+wsi_keyboard_reset(struct wsi_keyboard *keyboard)
+{
+    xkb_state_unref(keyboard->xkb_state);
+    keyboard->xkb_state = NULL;
+
+    xkb_keymap_unref(keyboard->xkb_keymap);
+    keyboard->xkb_keymap = NULL;
+}
+
+static bool
+wsi_keyboard_init_xkb(struct wsi_keyboard *keyboard, int fd, uint32_t size)
+{
+    char *map_str = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+    if (map_str == MAP_FAILED) {
+        return false;
+    }
+
+    struct xkb_keymap *keymap = xkb_keymap_new_from_string(
+        keyboard->xkb_context,
+        map_str,
+        XKB_KEYMAP_FORMAT_TEXT_V1,
+        XKB_KEYMAP_COMPILE_NO_FLAGS);
+    munmap(map_str, size);
+    if (!keymap) {
+        return false;
+    }
+
+    struct xkb_state *state = xkb_state_new(keymap);
+    if (!state) {
+        xkb_keymap_unref(keymap);
+        return false;
+    }
+
+    keyboard->xkb_keymap = keymap;
+    keyboard->xkb_state = state;
+    return true;
+}
+
+// region Wl Keyboard
+
+static void
+wl_keyboard_keymap(
+    void *data,
+    struct wl_keyboard *wl_keyboard,
+    uint32_t format,
+    int32_t fd,
+    uint32_t size)
+{
+    struct wsi_keyboard *keyboard = data;
+
+    wsi_keyboard_reset(keyboard);
+
+    if (format != WL_KEYBOARD_KEYMAP_FORMAT_XKB_V1) {
+        close(fd);
+        return;
+    }
+
+    wsi_keyboard_init_xkb(keyboard, fd, size);
+}
+
+static void
+wl_keyboard_enter(
+    void *data,
+    struct wl_keyboard *wl_keyboard,
+    uint32_t serial,
+    struct wl_surface *surface,
+    struct wl_array *keys)
+{
+}
+
+static void
+wl_keyboard_leave(
+    void *data,
+    struct wl_keyboard *wl_keyboard,
+    uint32_t serial,
+    struct wl_surface *surface)
+{
+}
+
+static void
+wl_keyboard_key(
+    void *data,
+    struct wl_keyboard *wl_keyboard,
+    uint32_t serial,
+    uint32_t time,
+    uint32_t key,
+    uint32_t state)
+{
+}
+
+static void
+wl_keyboard_modifiers(
+    void *data,
+    struct wl_keyboard *wl_keyboard,
+    uint32_t serial,
+    uint32_t mods_depressed,
+    uint32_t mods_latched,
+    uint32_t mods_locked,
+    uint32_t group)
+{
+    struct wsi_keyboard *keyboard = data;
+
+    if (keyboard->xkb_state) {
+        xkb_state_update_mask(
+            keyboard->xkb_state,
+            mods_depressed,
+            mods_latched,
+            mods_locked,
+            0,
+            0,
+            group);
+    }
+}
+
+static void
+wl_keyboard_repeat_info(
+    void *data,
+    struct wl_keyboard *wl_keyboard,
+    int32_t rate,
+    int32_t delay)
+{
+    struct wsi_keyboard *keyboard = data;
+
+    keyboard->repeat_rate = rate;
+    keyboard->repeat_delay = delay;
+}
+
+static const struct wl_keyboard_listener wl_keyboard_listener = {
+    .keymap = wl_keyboard_keymap,
+    .enter = wl_keyboard_enter,
+    .leave = wl_keyboard_leave,
+    .key = wl_keyboard_key,
+    .modifiers = wl_keyboard_modifiers,
+    .repeat_info = wl_keyboard_repeat_info,
+};
+
+// endregion
+
+static bool
+wsi_keyboard_create(struct wsi_seat *seat)
+{
+    assert(seat->keyboard == NULL);
+
+    struct wsi_keyboard *keyboard = calloc(1, sizeof(struct wsi_keyboard));
+    if (!keyboard) {
+        return false;
+    }
+
+    struct wsi_platform *platform = seat->global.platform;
+
+    keyboard->seat = seat;
+    keyboard->xkb_context = xkb_context_ref(platform->xkb_context);
+
+    keyboard->wl_keyboard = wl_seat_get_keyboard(seat->wl_seat);
+    wl_keyboard_add_listener(keyboard->wl_keyboard, &wl_keyboard_listener, keyboard);
+
+    seat->keyboard = keyboard;
+
+    return true;
+}
+
+static void
+wsi_keyboard_destroy(struct wsi_seat *seat)
+{
+    assert(seat->keyboard != NULL);
+
+    struct wsi_keyboard *keyboard = seat->keyboard;
+    seat->keyboard = NULL;
+
+    if (wl_keyboard_get_version(keyboard->wl_keyboard) >=
+        WL_KEYBOARD_RELEASE_SINCE_VERSION)
+    {
+        wl_keyboard_release(keyboard->wl_keyboard);
+    } else {
+        wl_keyboard_destroy(keyboard->wl_keyboard);
+    }
+
+    xkb_state_unref(keyboard->xkb_state);
+    xkb_keymap_unref(keyboard->xkb_keymap);
+    xkb_context_unref(keyboard->xkb_context);
+
+    free(keyboard);
+}
 
 // region WL Seat
 
--- src/wayland/input_priv.h
+++ src/wayland/input_priv.h
@@ -1,6 +1,67 @@
 #ifndef WSI_SRC_WAYLAND_INPUT_PRIVATE_H
 #define WSI_SRC_WAYLAND_INPUT_PRIVATE_H
 
+enum wsi_pointer_event {
+    WSI_POINTER_EVENT_NONE = 0,
+    WSI_POINTER_EVENT_ENTER = 1,
+    WSI_POINTER_EVENT_LEAVE = 2,
+    WSI_POINTER_EVENT_MOTION = 4,
+    WSI_POINTER_EVENT_BUTTON = 8,
+    WSI_POINTER_EVENT_AXIS = 16,
+    WSI_POINTER_EVENT_AXIS_SOURCE = 32,
+    WSI_POINTER_EVENT_AXIS_STOP = 64,
+    WSI_POINTER_EVENT_AXIS_DISCRETE = 128,
+};
+
+struct wsi_pointer_frame {
+    enum wsi_pointer_event mask;
+
+    uint32_t serial;
+    uint32_t time;
+
+    struct wl_surface *enter;
+    struct wl_surface *leave;
+
+    uint32_t button;
+    uint32_t state;
+
+    double x;
+    double y;
+
+    uint32_t axis_source;
+    struct {
+        double value;
+        int32_t discrete;
+        uint32_t start_time;
+        uint32_t stop_time;
+    } axes[2];
+};
+
+struct wsi_pointer {
+    struct wsi_seat        *seat;
+
+    struct wl_pointer      *wl_pointer;
+
+    struct wl_cursor_theme *wl_cursor_theme;
+    struct wl_cursor       *wl_cursor;
+    struct wl_surface      *wl_cursor_surface;
+
+    struct wsi_pointer_frame frame;
+};
+
+struct wsi_keyboard {
+    struct wsi_seat    *seat;
+
+    struct wl_keyboard *wl_keyboard;
+
+    struct xkb_context *xkb_context;
+    struct xkb_keymap  *xkb_keymap;
+    struct xkb_state   *xkb_state;
+
+    int32_t repeat_rate;
+    int32_t repeat_delay;
+};
+
 struct wsi_seat_ref {
     struct wl_list link;
     uint64_t id;
