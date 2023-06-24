482eee10b1f6b9f280efbae3f65e9d6db40ba4d5
Mark Bolhuis <mark@bolhuis.dev>
2023-06-24 21:28:56 +0100

    wayland: Move time functions to common_priv.h

--- src/wayland/common_priv.h
+++ src/wayland/common_priv.h
@@ -3,6 +3,12 @@
 
 #include "wsi/common.h"
 
+enum wsi_api {
+    WSI_API_NONE = 0,
+    WSI_API_EGL = 1,
+    WSI_API_VULKAN = 2,
+};
+
 static inline bool
 wsi_extent_equal(WsiExtent a, WsiExtent b)
 {
@@ -15,10 +21,23 @@ div_round(int32_t a, int32_t b)
     return (a + b / 2) / b;
 }
 
-enum wsi_api {
-    WSI_API_NONE = 0,
-    WSI_API_EGL = 1,
-    WSI_API_VULKAN = 2,
-};
+static inline int64_t
+wsi_tv_to_ns(uint32_t tv_sec_hi, uint32_t tv_sec_lo, uint32_t tv_nsec)
+{
+    int64_t tv_sec = ((int64_t)tv_sec_hi << 32) | (int64_t)tv_sec_lo;
+    return (tv_sec * 1000000000) + (int64_t)tv_nsec;
+}
+
+static inline int64_t
+wsi_us_to_ns(uint32_t utime_hi, uint32_t utime_lo)
+{
+    return (((int64_t)utime_hi) << 32 | (int64_t)utime_lo) * 1000;
+}
+
+static inline int64_t
+wsi_ms_to_ns(uint32_t mtime)
+{
+    return ((int64_t)mtime) * 1000000;
+}
 
 #endif
--- src/wayland/input.c
+++ src/wayland/input.c
@@ -15,29 +15,11 @@
 
 #include <xkbcommon/xkbcommon.h>
 
+#include "common_priv.h"
 #include "platform_priv.h"
 #include "input_priv.h"
 
 const uint32_t WSI_WL_SEAT_VERSION = 7;
-
-static inline int64_t
-wsi_tv_to_ns(uint32_t tv_sec_hi, uint32_t tv_sec_lo, uint32_t tv_nsec)
-{
-    int64_t tv_sec = ((int64_t)tv_sec_hi << 32) | (int64_t)tv_sec_lo;
-    return (tv_sec * 1000000000) + (int64_t)tv_nsec;
-}
-
-static inline int64_t
-wsi_us_to_ns(uint32_t utime_hi, uint32_t utime_lo)
-{
-    return (((int64_t)utime_hi) << 32 | (int64_t)utime_lo) * 1000;
-}
-
-static inline int64_t
-wsi_ms_to_ns(uint32_t mtime)
-{
-    return ((int64_t)mtime) * 1000000;
-}
 
 static struct wsi_seat *
 wsi_seat_find(struct wsi_platform *platform, uint64_t id)
