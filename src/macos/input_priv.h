27a81f33c0809521eeeeb7435e0ff8f41c9000dd
Samuliak <samuliak77@gmail.com>
2023-07-05 17:02:43 +0200

    macos: Initial Support

---
+++ src/macos/input_priv.h
@@ -0,0 +1,14 @@
+#ifndef WSI_SRC_COCOA_INPUT_PRIVATE_H
+#define WSI_SRC_COCOA_INPUT_PRIVATE_H
+
+#include "wsi/input.h"
+
+struct wsi_pointer {
+    //xcb_input_device_id_t device_id;
+};
+
+struct wsi_keyboard {
+    //xcb_input_device_id_t device_id;
+};
+
+#endif
